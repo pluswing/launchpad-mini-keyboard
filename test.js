f434e178dd3dff621bd86fc8ce59b2108be3a806
toshiyuki <toshiyuki.saito.16@gmail.com>
2021-11-26 08:41:34 +0900

    #04 LED制御サンプル

--- test.js
+++ test.js
@@ -25,6 +25,9 @@ inputMIDI.on('message', (deltaTime, message) => {
     // keyup -> 無視する
     return
   }
+  output.sendMessage([
+    0x80, note, 0x00
+  ])
   robot.typeString(`note: ${note}`);
 });
 
@@ -41,16 +44,54 @@ inputMIDI.openPort(1);
 inputDAW.ignoreTypes(false, false, false);
 inputMIDI.ignoreTypes(false, false, false);
 
-output.openPort(0);
+output.openPort(1); // MIDI
+
 // get Version
 // output.sendMessage([0xF0, 0x7E, 0x7F, 0x06, 0x01, 0xF7]);
 
-output.sendMessage([0xF0, 0x00, 0x20, 0x29, 0x02, 0x0D, 0x00, 0x05, 0xF7]);
+// change keys mode
+//output.sendMessage([0xF0, 0x00, 0x20, 0x29, 0x02, 0x0D, 0x00, 0x05, 0xF7]);
 
-output.closePort();
+const HEADER = [0xF0, 0x00, 0x20, 0x29, 0x02, 0x0D]
+const DELIMITER = [0xF7];
+output.sendMessage([
+  ...HEADER,
+  0x0E, // Programmer / Live mode change
+  0x01, // Programmer mode
+  ...DELIMITER
+])
+
+// // Lighting the lower left pad static red
+// output.sendMessage([
+//   0x90, 0x0B, 0x05
+// ])
+
+// // Flashing the upper left pad green
+// output.sendMessage([
+//   0x90, 0x51, 0x05
+// ])
+// output.sendMessage([
+//   0x91, 0x51, 0x13
+// ])
+
+// // Pulsing the lower right pad blue
+// output.sendMessage([
+//   0x92, 0x12, 0x2D
+// ])
+
+output.sendMessage([
+  ...HEADER,
+  0x03,
+  0x03, 0x0B, 127, 127, 127,
+  0x03, 0x0C, 127, 0, 0,
+  0x03, 0x0D, 0, 0, 127,
+  0x03, 0x0E, 0, 127, 0,
+  ...DELIMITER
+])
 
 process.on("SIGINT", () => {
   console.log("exit!")
+  output.closePort();
   inputMIDI.closePort()
   inputDAW.closePort()
   process.exit(0)
