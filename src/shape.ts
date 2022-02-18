306210e702dd504a42311dee59b526556552a0a4
toshiyuki <toshiyuki.saito.16@gmail.com>
2022-02-18 10:45:34 +0900

    #047 円の塗りつぶし完成

--- src/shape.ts
+++ src/shape.ts
@@ -1,6 +1,4 @@
-import { imageResource } from '@nut-tree/nut-js';
-import { fchmod } from 'fs';
-import { Color, newImage, Point, setPixel, stackImage } from './draw';
+import { Color, newImage, Point, setPixel } from './draw';
 import { range } from './util';
 
 export interface Circle {
@@ -9,10 +7,29 @@ export interface Circle {
   color: Color;
 }
 
+const unique = (list: number[]): number[] => {
+  return [...new Set(list)];
+};
+
 export const filledCircle = (c: Circle) => {
-  return range(Math.floor(c.r + 1)).reduce((img, r) => {
-    return stackImage(img, circleB({ ...c, r }));
-  }, newImage());
+  const points = circleBPints(c);
+  const image = newImage();
+  points.forEach((p) => {
+    setPixel(image, p.x, p.y, c.color);
+  });
+
+  const xList = unique(points.map((p) => p.x));
+  xList.forEach((x) => {
+    const ys = points.filter((p) => p.x == x).map((p) => p.y);
+    const count = ys.length;
+    if (count <= 1) return;
+    const minY = Math.min(...ys);
+    const maxY = Math.max(...ys);
+    for (let y = minY; y <= maxY; y++) {
+      setPixel(image, x, y, c.color);
+    }
+  });
+  return image;
 };
 
 export const circle = (c: Circle) => {
@@ -31,26 +48,34 @@ export const circle = (c: Circle) => {
 };
 
 export const circleB = (c: Circle) => {
+  const image = newImage();
+  circleBPints(c).forEach((p) => {
+    setPixel(image, p.x, p.y, c.color);
+  });
+  return image;
+};
+
+const circleBPints = (c: Circle): Point[] => {
   // ブレゼンハム
   let cx = 0,
     cy = 0,
     d = 0;
 
-  const radius = c.r;
+  const radius = Math.ceil(c.r);
 
   cx = 0;
   cy = radius;
   d = 2 - 2 * radius;
 
-  const hdc = newImage();
   const center = c.center;
-  const col = c.color;
+
+  const points: Point[] = [];
 
   // 開始点の描画
-  setPixel(hdc, cx + center.x, cy + center.y, col); //  (0, R);
-  setPixel(hdc, cx + center.x, -cy + center.y, col); //  (0, -R);
-  setPixel(hdc, cy + center.x, cx + center.y, col); //  (R, 0);
-  setPixel(hdc, -cy + center.x, cx + center.y, col); //  (-R, 0);
+  points.push({ x: cx + center.x, y: cy + center.y }); //  (0, R);
+  points.push({ x: cx + center.x, y: -cy + center.y }); //  (0, -R);
+  points.push({ x: cy + center.x, y: cx + center.y }); //  (R, 0);
+  points.push({ x: -cy + center.x, y: cx + center.y }); //  (-R, 0);
 
   for (;;) {
     if (d > -cy) {
@@ -66,10 +91,10 @@ export const circleB = (c: Circle) => {
     if (cy <= 0) break; // 描画終了
 
     // 描画
-    setPixel(hdc, cx + center.x, cy + center.y, col); // 0～90度の間
-    setPixel(hdc, -cx + center.x, cy + center.y, col); // 90～180度の間
-    setPixel(hdc, -cx + center.x, -cy + center.y, col); // 180～270度の間
-    setPixel(hdc, cx + center.x, -cy + center.y, col); // 270～360度の間
+    points.push({ x: cx + center.x, y: cy + center.y }); // 0～90度の間
+    points.push({ x: -cx + center.x, y: cy + center.y }); // 90～180度の間
+    points.push({ x: -cx + center.x, y: -cy + center.y }); // 180～270度の間
+    points.push({ x: cx + center.x, y: -cy + center.y });
   }
-  return hdc;
+  return points;
 };
