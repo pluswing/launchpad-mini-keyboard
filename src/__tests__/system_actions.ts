f522860271960f09b47670a931e12e0ffd73754a
toshiyuki <toshiyuki.saito.16@gmail.com>
2022-04-20 11:04:29 +0900

    #84 テストを修正

--- src/__tests__/system_actions.ts
+++ src/__tests__/system_actions.ts
@@ -1,14 +1,29 @@
-import { execSync } from 'child_process';
+import child_process from 'child_process';
 import { launchApp } from 'system_actions';
-jest.mock('child_process');
+import * as util from '../util';
 
 describe(`${__filename}`, () => {
-  it('launchApp', () => {
-    const mockExecSync = execSync as jest.MockedFunction<typeof execSync>;
+  it('launchApp (mac)', () => {
+    const mock = jest.spyOn(child_process, 'execSync');
+    mock.mockClear();
+    mock.mockReturnValue('');
+    jest.spyOn(util, 'isMac').mockReturnValue(true);
 
     launchApp('path to app');
 
-    expect(mockExecSync).toHaveBeenCalled();
-    expect(mockExecSync.mock.calls[0][0]).toBe('open "path to app"');
+    expect(mock).toHaveBeenCalled();
+    expect(mock.mock.calls[0][0]).toBe('open "path to app"');
+  });
+
+  it('launchApp (win)', () => {
+    const mock = jest.spyOn(child_process, 'execSync');
+    mock.mockClear();
+    mock.mockReturnValue('');
+    jest.spyOn(util, 'isMac').mockReturnValue(false);
+
+    launchApp('path to app');
+
+    expect(mock).toHaveBeenCalled();
+    expect(mock.mock.calls[0][0]).toBe('"path to app"');
   });
 });
