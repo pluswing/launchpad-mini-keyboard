661576177a5cb287d8e7760104d671fad4dab55e
pluswing <toshiyuki@pluswing.co.jp>
2022-06-20 15:10:42 +0900

    windowsキーストローク修正
    - spaceなど特殊キーが未対応だったのを修正
    - super(windows)キー対応

--- src/system_actions.ts
+++ src/system_actions.ts
@@ -49,8 +49,58 @@ const typeKeyboardForWindows = async (s: ShortcutKey): Promise<void> => {
     super: Key.LeftSuper,
   };
   const sk = s.specialKeys.map((k) => keymap[k]);
+
+  const keymap2: { [key: string]: number } = {
+    space: Key.Space,
+    esc: Key.Escape,
+    tab: Key.Tab,
+    f1: Key.F1,
+    f2: Key.F2,
+    f3: Key.F3,
+    f4: Key.F4,
+    f5: Key.F5,
+    f6: Key.F6,
+    f7: Key.F7,
+    f8: Key.F8,
+    f9: Key.F9,
+    f10: Key.F10,
+    f11: Key.F11,
+    f12: Key.F12,
+    f13: Key.F13,
+    f14: Key.F14,
+    f15: Key.F15,
+    f16: Key.F16,
+    f17: Key.F17,
+    f18: Key.F18,
+    f19: Key.F19,
+    f20: Key.F20,
+    f21: Key.F21,
+    f22: Key.F22,
+    f23: Key.F23,
+    f24: Key.F24,
+    left: Key.Left,
+    up: Key.Up,
+    right: Key.Right,
+    down: Key.Down,
+    print: Key.Print,
+    pause: Key.Pause,
+    insert: Key.Insert,
+    delete: Key.Delete,
+    home: Key.Home,
+    end: Key.End,
+    'page up': Key.PageUp,
+    'page down': Key.PageDown,
+    enter: Key.Enter,
+    backspace: Key.Backspace,
+  };
+
+  console.log({ s, sk, key: s.key });
   await keyboard.pressKey(...sk);
-  await keyboard.type(s.key);
+  if (keymap2[s.key.toLowerCase()] !== undefined) {
+    await keyboard.type(keymap2[s.key.toLowerCase()]);
+  } else {
+    await keyboard.type(s.key);
+  }
   await keyboard.releaseKey(...sk);
 };
 
